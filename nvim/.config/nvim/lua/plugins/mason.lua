f87cfa76ae2782cd03f2e8d33c8609878428674f
Muhamad Gameel <mgameel@pm.me>
2024-08-17 23:57:25 +0300

    nvim: add 'mason' plugin to download language servers

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,43 @@
+return {
+  'williamboman/mason.nvim',
+  event = { 'BufRead', 'BufNewFile' },
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
+  config = function()
+    require('mason').setup {
+      ui = {
+        border = 'rounded',
+        icons = {
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗',
+        },
+      },
+    }
+
+    require('mason-lspconfig').setup {}
+
+    require('mason-tool-installer').setup {
+      ensure_installed = {
+        -- LSP
+        'lua-language-server',
+        'css-lsp',
+        'html-lsp',
+        'json-lsp',
+        'eslint-lsp',
+        'bash-language-server',
+
+        -- Linters
+        'shellcheck', -- shell scripts
+        'eslint_d',
+
+        -- Formatters
+        'prettierd',
+        'stylua', -- lua
+        'shfmt', -- shell script
+      },
+    }
+  end,
+}
