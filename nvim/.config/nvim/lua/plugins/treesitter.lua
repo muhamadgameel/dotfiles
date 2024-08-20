f4695825bcec346740cf075a3f1c9081c56cd112
Muhamad Gameel <mgameel@pm.me>
2024-08-21 00:07:51 +0300

    nvim: add 'TreeSJ' for splitting and joining blocks of code

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,133 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  event = { 'BufReadPost', 'BufNewFile' },
-  lazy = false,
-  dependencies = {
-    { 'windwp/nvim-ts-autotag' },
-    { 'RRethy/nvim-treesitter-endwise' },
-    { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  --------------
+  -- Treesitter
+  --------------
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = { 'BufReadPost', 'BufNewFile' },
+    dependencies = {
+      { 'windwp/nvim-ts-autotag' },
+      { 'RRethy/nvim-treesitter-endwise' },
+      { 'nvim-treesitter/nvim-treesitter-textobjects' },
+      config = function()
+        local configs = require 'nvim-treesitter.configs'
+
+        vim.opt.foldmethod = 'expr'
+        vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+        configs.setup {
+          ensure_installed = {
+            'tsx',
+            'html',
+            'css',
+            'jsdoc',
+            'json',
+            'json5',
+            'jsonc',
+            'javascript',
+            'typescript',
+            'tsx',
+            'lua',
+            'vim',
+            'vimdoc',
+            'query',
+            'bash',
+            'python',
+            'c',
+            'cpp',
+            'rust',
+            'go',
+            'toml',
+            'yaml',
+            'git_config',
+            'gitcommit',
+            'gitignore',
+            'markdown',
+            'markdown_inline',
+            'ruby',
+            'sql',
+            'regex',
+          },
+          sync_install = false,
+          auto_install = true,
+          highlight = {
+            enable = true,
+            disable = {},
+            additional_vim_regex_highlighting = false,
+          },
+          autotag = { enable = true },
+          indent = { enable = true },
+          endwise = { enable = true },
+
+          -- Incremental selection configuration
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = '<CR>',
+              node_incremental = '<CR>',
+              node_decremental = '<BS>',
+              scope_incremental = '<TAB>',
+            },
+          },
+
+          -- Textobjects configuration
+          textobjects = {
+            select = {
+              enable = true,
+              lookahead = true,
+              keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+                ['al'] = '@loop.outer',
+                ['il'] = '@loop.inner',
+                ['aa'] = '@parameter.outer',
+                ['ia'] = '@parameter.inner',
+              },
+            },
+            move = {
+              enable = true,
+              set_jumps = true, -- whether to set jumps in the jumplist
+              goto_next_start = {
+                [']m'] = '@function.outer',
+                [']]'] = '@class.outer',
+              },
+              goto_next_end = {
+                [']M'] = '@function.outer',
+                [']['] = '@class.outer',
+              },
+              goto_previous_start = {
+                ['[m'] = '@function.outer',
+                ['[['] = '@class.outer',
+              },
+              goto_previous_end = {
+                ['[M'] = '@function.outer',
+                ['[]'] = '@class.outer',
+              },
+            },
+            swap = {
+              enable = true,
+              swap_next = {
+                ['<leader>a'] = '@parameter.inner',
+              },
+              swap_previous = {
+                ['<leader>A'] = '@parameter.inner',
+              },
+            },
+          },
+        }
+      end,
+    },
+
+    ------------
+    -- Comments
+    ------------
     {
-      ------------
-      -- Comments
-      ------------
       'numToStr/Comment.nvim',
+      event = { 'BufReadPost', 'BufNewFile' },
       dependencies = {
         { 'JoosepAlviste/nvim-ts-context-commentstring' },
       },
@@ -28,11 +144,13 @@ return {
         }
       end,
     },
+
+    -----------------------------------
+    -- JS/TS template string converter
+    -----------------------------------
     {
-      -----------------------------------
-      -- JS/TS template string converter
-      -----------------------------------
       'axelvc/template-string.nvim',
+      event = { 'BufReadPost', 'BufNewFile' },
       config = function()
         require('template-string').setup {
           filetypes = {
@@ -55,117 +173,22 @@ return {
       end,
     },
   },
-  --------------
-  -- Treesitter
-  --------------
-  config = function()
-    local configs = require 'nvim-treesitter.configs'
 
-    vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+  ---------------------------------------------
+  -- TreeSJ (splitting/joining blocks of code)
+  ---------------------------------------------
+  {
+    'Wansmer/treesj',
+    event = { 'BufReadPost', 'BufNewFile' },
+    config = function()
+      local treesj = require 'treesj'
 
-    configs.setup {
-      ensure_installed = {
-        'tsx',
-        'html',
-        'css',
-        'jsdoc',
-        'json',
-        'json5',
-        'jsonc',
-        'javascript',
-        'typescript',
-        'tsx',
-        'lua',
-        'vim',
-        'vimdoc',
-        'query',
-        'bash',
-        'python',
-        'c',
-        'cpp',
-        'rust',
-        'go',
-        'toml',
-        'yaml',
-        'git_config',
-        'gitcommit',
-        'gitignore',
-        'markdown',
-        'markdown_inline',
-        'ruby',
-        'sql',
-        'regex',
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = {},
-        additional_vim_regex_highlighting = false,
-      },
-      autotag = { enable = true },
-      indent = { enable = true },
-      endwise = { enable = true },
+      treesj.setup { use_default_keymaps = false }
 
-      -- Incremental selection configuration
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<CR>',
-          node_incremental = '<CR>',
-          node_decremental = '<BS>',
-          scope_incremental = '<TAB>',
-        },
-      },
-
-      -- Textobjects configuration
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['al'] = '@loop.outer',
-            ['il'] = '@loop.inner',
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
-      },
-    }
-  end,
+      vim.keymap.set('n', '<leader>m', treesj.toggle)
+      vim.keymap.set('n', '<leader>M', function()
+        treesj.toggle { split = { recursive = true } }
+      end)
+    end,
+  },
 }
