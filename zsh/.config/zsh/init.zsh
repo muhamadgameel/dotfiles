590131372119c0a4b73bee70764cf6e9fb9124e1
Muhamad Gameel <mgameel@pm.me>
2024-11-13 01:58:24 +0300

    zsh: add zpm plugins manager

--- zsh/.config/zsh/init.zsh
+++ zsh/.config/zsh/init.zsh
@@ -13,3 +13,19 @@ sources=(
 for src in ${sources[@]}; do
   source ${0:a:h}/core/$src.zsh
 done
+
+# load Zpm package manager
+source ${0:a:h}/zpm.zsh
+
+# Add plugins
+Plug "zdharma-continuum/fast-syntax-highlighting"
+Plug "zsh-users/zsh-autosuggestions"
+Plug "zsh-users/zsh-completions"
+Plug "zsh-users/zsh-history-substring-search"
+
+# History substring search
+bindkey -M emacs "${key_info[Up]}" history-substring-search-up
+bindkey -M emacs "${key_info[Down]}" history-substring-search-down
+
+# Auto Suggestions
+ZSH_AUTOSUGGEST_STRATEGY=(history)
