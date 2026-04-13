20583b70caaddffdff7f7ae42cd006922913f050
Muhamad Gameel <mgameel@pm.me>
2026-04-13 21:08:09 +0300

    nvim: add which-key.nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,21 @@
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {
+    preset = 'helix',
+    delay = 300,
+    spec = {
+      { '<leader>b', group = 'buffer' },
+      { '<leader>c', group = 'code' },
+      { '<leader>h', group = 'git hunk' },
+      { '<leader>o', group = 'open/search' },
+      { '<leader>og', group = 'git' },
+      { '<leader>p', group = 'swap param' },
+      { '<leader>r', group = 'rename/replace' },
+      { '<leader>s', group = 'split/session' },
+      { '<leader>t', group = 'toggle' },
+      { '<leader>x', group = 'trouble' },
+      { 'gr', group = 'LSP goto/actions' },
+    },
+  },
+}
